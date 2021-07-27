e563124bdd59a2fa1b0156eca73018a5d1496cb3
李梓豪 <yidianyiko@foxmail.com>
2021-07-27 13:22:11 +0800

    first math

---
+++ include/yutil/math.h
@@ -0,0 +1,14 @@
+#ifndef UTIL_MATH_H
+#define UTIL_MATH_H
+
+#ifndef max
+#define max(x, y) (((x) > (y)) ? (x) : (y))
+#endif
+
+#ifndef min
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+#endif
+
+#define round(x) ((x) > 0 ? (int)((x) + 0.5) : (int)((x)-0.5))
+
+#endif /* UTIL_MATH_H */
